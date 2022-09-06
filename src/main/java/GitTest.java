eca6fb9b7f3a5da67a64a70b3854df247a60e4fa
CSZ <CSZ@163.com>
2022-09-06 19:47:44 +0800

    hot-fix second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git1");
@@ -5,4 +7,12 @@ public class GitTest {
         System.out.println("git3");
         System.out.println("hot-fix");
     }
+    @Test
+    public void test(){
+        int num = 1_111_000;
+        System.out.println(num);
+
+
+    }
+
 }
